629210cf0af5f6c30217a8ff129a7c450a31ce4b
Maria <maria.altyeva@gmail.com>
2020-07-16 18:10:55 +0800

    added form

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -9,15 +9,22 @@ import Rails from '@rails/ujs';
 
 
 export default class extends Controller {
-  static targets = ['calendar']
+  static targets = ['calendar', "modal", "start_time", "end_time"]
 
   connect() {
+    let that = this;
     let calendar = new Calendar(this.calendarTarget, {
       events: '/events.json', 
       editable: true, 
       navLinks: true, 
       headerToolbar: {center: 'dayGridMonth, timeGridWeek, timeGridDay' }, 
-      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin]
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin], 
+      navLinkDayClick: function(date, jsEvent) {
+        that.modalTarget.style.display = "block";
+        that.start_timeTarget.value = date
+        that.end_timeTarget.value = date
+
+      }
     })
     calendar.render()
   }
